635217b86379799679b9b9a80c6ac432aa469b98
Raul <crazyraul7@gmail.com>
2021-02-14 08:56:53 -0800

    Adding the files

---
+++ app.js
@@ -0,0 +1,37 @@
+//N0gGSre32HbnqqqSAOnVRXuMM2mCfGRz
+
+
+async function getGiphy(search) {     
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=N0gGSre32HbnqqqSAOnVRXuMM2mCfGRz&limit=1`);
+    console.log(res);
+    addGiphy(res.data.data);
+    return res;
+}
+
+const button = document.getElementById('button');
+const remove = document.getElementById('delete');
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let value = document.getElementById('input').value;
+    getGiphy(value);
+    document.getElementById('input').value = "";
+    return null;
+});
+
+remove.addEventListener('click', (e) => {
+    e.preventDefault();
+    const section = document.getElementById('answers');
+    section.innerHTML = "";
+    return null;
+});
+
+function addGiphy(results) {
+    const section = document.getElementById('answers');
+    let giphy = document.createElement('img');
+    let url = results[0].images.downsized_medium.url;
+    giphy.className = 'giphy';
+    giphy.setAttribute('src', url);
+    section.append(giphy);
+    return;
+}
